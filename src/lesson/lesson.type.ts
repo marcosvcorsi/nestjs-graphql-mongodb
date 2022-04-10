8d1a5e2fdac454bd425ae6e5c6e5648f302d6663
Marcos Corsi <marcosvcorsi@gmail.com>
2022-04-10 15:28:09 -0300

    feat: add validation

--- src/lesson/lesson.type.ts
+++ src/lesson/lesson.type.ts
@@ -1,4 +1,5 @@
 import { Field, ID, InputType, ObjectType } from '@nestjs/graphql';
+import { IsDate, MinLength } from 'class-validator';
 
 @ObjectType('Lesson')
 export class LessonType {
@@ -18,11 +19,14 @@ export class LessonType {
 @InputType()
 export class CreateLessonInput {
   @Field()
+  @MinLength(1)
   name: string;
 
   @Field(() => Date)
+  @IsDate()
   startDate: Date;
 
   @Field(() => Date)
+  @IsDate()
   endDate: Date;
 }
